d6d5a776cb7184467f0c7ffe048c54a9f2eb9474
panando00 <prosperzvidzai00@gmail.com>
2025-01-13 21:26:24 +0200

    third commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        int guess;
+        bool isCorrect = false;
+        
+        while (!isCorrect)
+        {
+            Console.Write("What is your guess? ");
+            string userInput = Console.ReadLine();
+            if (int.TryParse(userInput, out guess))
+            {
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    isCorrect = true;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+        }
     }
 }
