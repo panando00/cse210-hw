0e2d05a43ef3ef99849de753448422578deb09d0
panando00 <prosperzvidzai00@gmail.com>
2025-01-13 14:58:47 +0200

    second commit

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter your grade percentage: ");
+        string userinput = Console.ReadLine();
+        int grade = int.Parse(userinput);
+        string letter = "";
+
+        //determining letter for each grade
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+            
+        // printing the letter grade
+        Console.WriteLine($"Your letter grade is: {letter}");
+            
+        // checking if the student passed and display the approprite message
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations. You passed the course!");
+        }
+        else
+        {
+        Console.WriteLine("Keep working hard. You'll do better next time!");
+        }
+        
     }
 }
